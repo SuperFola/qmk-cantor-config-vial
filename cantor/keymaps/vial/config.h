89c2ea843a6365d1b04953796c7ef8f3375fef61
Alexandre Plateau <lexplt.dev@gmail.com>
2023-03-04 15:53:19 +0100

    updating config to ignore mod tap interrupt

--- cantor/keymaps/vial/config.h
+++ cantor/keymaps/vial/config.h
@@ -1,12 +1,19 @@
 
-/* keyboard uid */
+// keyboard uid
 #define VIAL_KEYBOARD_UID {0xF0, 0x67, 0x2F, 0x54, 0x23, 0xB3, 0x7B, 0x55}
 
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
 
-/* default layer count */
+// default layer count
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
-/* tapping term in ms */
-#define TAPPING_TERM 180
+// tapping term in ms
+#define TAPPING_TERM 200
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef PERMISSIVE_HOLD
+#undef HOLD_ON_OTHER_KEY_PRESS
+
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2000
--- cantor/keymaps/vial/rules.mk
+++ cantor/keymaps/vial/rules.mk
@@ -1,4 +1,13 @@
 VIA_ENABLE  = yes    # Enable VIA
 VIAL_ENABLE = yes    # Enable VIAL
+
 CAPS_WORD_ENABLE = yes
 
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = yes          # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no
