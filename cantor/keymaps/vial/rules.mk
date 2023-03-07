29ed705b0cbefac2ee0c08a2b5fafaeed42e9860
Alexandre Plateau <lexplt.dev@gmail.com>
2023-03-07 11:26:48 +0100

    adding support for audio/system keys

--- cantor/keymaps/vial/rules.mk
+++ cantor/keymaps/vial/rules.mk
@@ -4,7 +4,7 @@ VIAL_ENABLE = yes    # Enable VIAL
 CAPS_WORD_ENABLE = yes
 
 MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
+EXTRAKEY_ENABLE = yes      # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes          # Enable N-Key Rollover
