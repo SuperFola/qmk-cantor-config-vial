053523857bec88f7cbebea89f21648903120520e
Alexandre Plateau <lexplt.dev@gmail.com>
2023-03-14 17:08:03 +0100

    adding mouse key support and moving numpad layer toggle to home row

--- cantor/keymaps/vial/keymap.c
+++ cantor/keymaps/vial/keymap.c
@@ -67,8 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // base layout
     [0] = LAYOUT_split_3x6_3(
         KC_DEL,     KC_Q,       KC_W,        KC_E,        KC_R,        KC_T,            KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,       KC_CAPS,
-        XXXXXXX,    OPT_T(KC_A),SFT_T(KC_S), CMD_T(KC_D), CTL_T(KC_F), LT(1, KC_G),     LT(1, KC_H), CTL_T(KC_J), CMD_T(KC_K), SFT_T(KC_L), KC_SCLN,    KC_LALT,
-        MO(4),      KC_Z,       KC_X,        KC_C,        KC_V,        KC_B,            KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,    XXXXXXX,
+        MO(4),      OPT_T(KC_A),SFT_T(KC_S), CMD_T(KC_D), CTL_T(KC_F), LT(1, KC_G),     LT(1, KC_H), CTL_T(KC_J), CMD_T(KC_K), SFT_T(KC_L), KC_SCLN,    KC_LALT,
+        XXXXXXX,    KC_Z,       KC_X,        KC_C,        KC_V,        KC_B,            KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,    XXXXXXX,
                                           LT(3, KC_TAB),  KC_SPC,      KC_ESC,          OSL(2),      KC_BSPC,     KC_ENT
     ),
 
--- cantor/keymaps/vial/rules.mk
+++ cantor/keymaps/vial/rules.mk
@@ -3,7 +3,7 @@ VIAL_ENABLE = yes    # Enable VIAL
 
 CAPS_WORD_ENABLE = yes
 
-MOUSEKEY_ENABLE = no       # Mouse keys
+MOUSEKEY_ENABLE = yes      # Mouse keys
 EXTRAKEY_ENABLE = yes      # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
